95ae8902d5731701d1694685533204d69623ef56
Gustavo Henke <guhenke@gmail.com>
2017-02-22 14:45:53 -0300

    Refactor to ES5
    So that old browsers still work

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 module.exports = exports = {};
-exports.createRequestReducer = ({ begin, success, failure }) => {
-    return ( state = {}, action ) => {
+exports.createRequestReducer = function ( actions ) {
+    return function ( state, action ) {
         switch ( action.type ) {
-            case begin:
+            case actions.begin:
                 return Object.assign( {}, state, {
                     loading: true
                 });
 
-            case success:
+            case actions.success:
                 return Object.assign( {}, state, {
                     loading: false,
                     data: action.payload
                 });
 
-            case failure:
+            case actions.failure:
                 return Object.assign( {}, state, {
                     loading: false,
                     error: action.payload
                 });
 
             default:
-                return state;
+                return state || {};
         }
     };
 };
 
-exports.createRequestSelectors = ( baseStateSelector, { isLoading, getData, getError }) => ({
-    [ isLoading ]: state => baseStateSelector( state ).loading,
-    [ getData ]: state => baseStateSelector( state ).data,
-    [ getError ]: state => baseStateSelector( state ).error
-});
+exports.createRequestSelectors = function ( baseStateSelector, selectorNames ) {
+    const selectors = {};
+    selectors[ selectorNames.isLoading ] = function ( state ) {
+        return baseStateSelector( state ).loading;
+    };
+
+    selectors[ selectorNames.getData ] = function ( state ) {
+        return baseStateSelector( state ).data;
+    };
+
+    selectors[ selectorNames.getError ] = function ( state ) {
+        return baseStateSelector( state ).error;
+    };
+    
+    return selectors;
+};
